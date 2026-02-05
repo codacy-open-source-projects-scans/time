d2b408b8c83f7d515c54087d2b9ba9f3aecabdb1
Codacy Archive <codacy-archive@local>
2026-02-05 01:16:38 +0000

    snapshot 2026-02-05T01:16:38.252427+00:00

--- src/time.c
+++ src/time.c
@@ -210,19 +210,11 @@ usage (int status)
 
   printf (_("\
 Usage: %s [OPTIONS] COMMAND [ARG]...\n\
-"),program_name);
+"), program_name);
   fputs (_("\
 Run COMMAND, then print system resource usage.\n\
 \n\
 "), stdout);
-
-  /*
-    printf ("\
-Usage: %s [-apvV] [-f format] [-o file] [--append] [--verbose]\n\
-       [--portability] [--format=format] [--output=file] [--version]\n\
-       [--help] command [arg...]\n",
-          program_name);
-  */
 
   fputs (_("\
   -a, --append              with -o FILE, append instead of overwriting\n\
@@ -234,9 +226,9 @@ Usage: %s [-apvV] [-f format] [-o file] [--append] [--verbose]\n\
   -o, --output=FILE         write to FILE instead of STDERR\n"), stdout);
   fputs (_("\
   -p, --portability         print POSIX standard 1003.2 conformant string:\n\
-                              real %%e\n\
-                              user %%U\n\
-                              sys %%S\n\
+                              real %e\n\
+                              user %U\n\
+                              sys %S\n\
 "), stdout);
   fputs (_("\
   -q, --quiet               do not print information about abnormal program\n\
